b2cce6f719cadda1df883ae5846e78a941bc6604
Pablo Martin Correa <pmartincorrea@gmail.com>
2024-05-04 02:07:49 -0300

    commit 04/05/2024.003

---
+++ M09_errorhandling/__ init__.py
@@ -0,0 +1,6 @@
+# Solución para: ModuleNotFoundError: No module named 'modulo'
+
+#Ya encontre la solucion, por si a alguien mas le sucede lo mismo, 
+#tienen que agregar el archivo __ init__.py (puede estar vacio) 
+#a ambas carpetas, tanto donde esta el archivo que importa como 
+#donde esta el que es importado
